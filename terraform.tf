0dc915f4642ca742537f051837cc001b02fe41e7
Aristide FOTSO <fotso02442gmail.com>
2023-06-03 21:39:03 +0100

    Remove Cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  /*cloud {
+    organization = "Terraform-Associate-Training-Aristide"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
-  }
+  }*/
 
   required_providers {
     aws = {
